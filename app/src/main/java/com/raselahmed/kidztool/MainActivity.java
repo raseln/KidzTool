960e8d62aff99e29ce3ab2da9cb7d5ecc41a26cb
Zahir261 <zahirulislam261@gmail.com>
2017-11-04 00:00:29 +0600

    Third Commit

--- app/src/main/java/com/raselahmed/kidztool/MainActivity.java
+++ app/src/main/java/com/raselahmed/kidztool/MainActivity.java
@@ -19,71 +19,23 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etEmail,etPassword;
-    TextView tvLink;
-    Button btnReg;
-    private ProgressDialog progressBar;
-    private FirebaseAuth firebaseAuth;
+    Button btnRegiser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar =new ProgressDialog(this);
-        firebaseAuth = FirebaseAuth.getInstance();
+        btnRegiser = (Button) findViewById(R.id.RegisterActivity);
 
-        etEmail = (EditText) findViewById(R.id.etEmail);
-        etPassword = (EditText) findViewById(R.id.etPassword);
-        btnReg = (Button) findViewById(R.id.btnRegister);
-        tvLink = (TextView) findViewById(R.id.tvLoginLink);
-
-        btnReg.setOnClickListener(new View.OnClickListener() {
+        btnRegiser.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                registerUser();
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
             }
         });
 
-        tvLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-            }
-        });
     }
 
-    private void registerUser() {
-        String email = etEmail.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
 
-        if (TextUtils.isEmpty(email)){
-            Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        progressBar.setTitle("Registering user...");
-        progressBar.show();
-
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(getApplicationContext(), "Registration successful.", Toast.LENGTH_SHORT).show();
-                    finish();
-                    startActivity(new Intent(getApplicationContext(), Profile.class));
-                }else {
-                    Toast.makeText(getApplicationContext(), "Registration failed.", Toast.LENGTH_SHORT).show();
-                    //Log.e("Error", task.toString());
-                }
-
-                progressBar.dismiss();
-            }
-        });
-    }
 }
---
+++ app/src/main/java/com/raselahmed/kidztool/RegistrationActivity.java
@@ -0,0 +1,89 @@
+package com.raselahmed.kidztool;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegistrationActivity extends AppCompatActivity {
+    EditText etEmail,etPassword;
+    TextView tvLink;
+    Button btnReg;
+    private ProgressDialog progressBar;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        progressBar =new ProgressDialog(this);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnReg = (Button) findViewById(R.id.btnRegister);
+        tvLink = (TextView) findViewById(R.id.tvLoginLink);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+
+        tvLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+    }
+
+    private void registerUser() {
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressBar.setTitle("Registering user...");
+        progressBar.show();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Registration successful.", Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), Profile.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Registration failed.", Toast.LENGTH_SHORT).show();
+                            //Log.e("Error", task.toString());
+                        }
+
+                        progressBar.dismiss();
+                    }
+                });
+    }
+}
